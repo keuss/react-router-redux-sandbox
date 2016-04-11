719cd63f9914196d8b83bfb98b4e299538da5002
guillaume gallois <guillaume.gallois@gmail.com>
2016-04-11 17:32:46 +0200

    update gitignore

---
+++ components/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link, browserHistory } from 'react-router'
+
+export default function App({ children }) {
+  console.log('render App with', children)
+  return (
+    <div>
+      <header>
+        Links:
+        {' '}
+        <Link to="/">Home</Link>
+        {' '}
+        <Link to="/foo">Foo</Link>
+        {' '}
+        <Link to="/bar">Bar</Link>
+      </header>
+      <div>
+        <button onClick={() => browserHistory.push('/foo')}>Go to /foo</button>
+      </div>
+      <div style={{ marginTop: '1.5em' }}>{children}</div>
+    </div>
+  )
+}
